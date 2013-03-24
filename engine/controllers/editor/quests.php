00ceed66d64c2c043cb19fefcb9231661aaf7c0e
bels <bels@daphne.belfield.us>
2013-03-24 18:16:02 -0400

    - Fixed htaccess file - The beginnings of the form for creating quests.  The inserting part still needs to be added and I need to figure out if bootstrap really has a textarea or not.  Wanted others to be able to hack on this as well.

--- engine/controllers/editor/quests.php
+++ engine/controllers/editor/quests.php
@@ -13,5 +13,23 @@ class Quests extends MY_Controller {
 		$this->data['quests'] = $this->quests->get_all();
                 $this->data['meta_title'] = 'All Quests';
 	}
-	
+
+	public function create()
+	{
+		$this->form_validation->set_rules('name','Name','required|trim|xss_clean');
+		$this->form_validation->set_rules('verbiage','Quest Text','required|trim|xss_clean');
+		$this->form_validation->set_rules('giver','Quest Giver','required|trim|xss_clean');
+		$this->form_validation->set_rules('turnin','Quest Turnin','required|trim|xss_clean');
+		$this->form_validation->set_rules('monetary_reward','Monetary Reward','trim|xss_clean');
+		$this->form_validation->set_rules('item_reward','Item Reward','trim|xss_clean');
+		$this->form_validation->set_rules('experience','Experience','trim|xss_clean');
+
+		if ($this->form_validation->run() == TRUE && $this->quests->insert($_POST))
+                {
+                        // Creating the characters was successful, redirect them back to the admin page
+                        flashmsg('Quest created successfully.', 'success');
+                        redirect('/editor/quests');
+                }
+		$this->data['meta_title'] = 'Create Quest';
+	}	
 }
---
+++ engine/views/editor/quests/create.php
@@ -0,0 +1,20 @@
+<style>body, html { overflow:hidden; }</style>
+<div class="pane">
+        <div class="page-header">
+                <h1>Create Quests</h1>
+        </div>
+	<?=form_open(current_url(), 'class="form-horizontal"')?>
+                <?=bootstrap_input('name', 'Name')?>
+                <?=form_textarea('verbiage', 'Quest Text')?>
+                <?=bootstrap_input('giver', 'Quest Giver')?>
+                <?=bootstrap_input('turnin', 'Quest Turnin')?>
+                <?=bootstrap_input('monetary_reward', 'Monetary Reward')?>
+                <?=bootstrap_input('item_reward', 'Item Reward')?>
+                <?=bootstrap_input('experience', 'Experience')?>
+                <div class="form-actions">
+                        <?=bootstrap_submit('submit', 'Create Quest', 'class="btn btn-primary"')?>
+                        <a href="<?=base_url('editor/quests')?>" class="btn">Cancel</a>
+                </div>
+        <?=form_close()?>
+</div>
+
